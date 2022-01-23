9890d1e740569c0d5fd9e11e332bc23d1b46919d
jorgehauck <jorge.hauck1997@outlook.com>
2022-01-23 12:53:42 -0300

    Criação de rodapé

--- frontend/src/components/Rodape/index.tsx
+++ frontend/src/components/Rodape/index.tsx
@@ -1,16 +1,15 @@
 import './styles.css';
-
+import linkedin from 'assets/img/linkedin.svg';
+import github from 'assets/img/github-footer.svg';
 function Footer() {
     return (
-       
         <footer id="myFooter">
             <div className="container">
                 <p className="footer-copyright">© 2022 Copyright - Projeto Criado por: José Jorge Hauck Júnior</p>
             </div>
             <div className="footer-social">
-                
-                <a href="https://www.linkedin.com/in/jorgehauck/" className="social-icons"><i className="fa fa-linkedin"></i></a>
-                <a href="https://github.com/jorgehauck" className="social-icons"><i className="fa fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/jorgehauck/" target="_blank" rel="noreferrer"><img src={linkedin} alt="" className='linkedin'/></a>
+                <a href="https://github.com/jorgehauck" target="_blank" rel="noreferrer"><img src={github} alt="github" className='github'/></a>
             </div>
         </footer>
     )
