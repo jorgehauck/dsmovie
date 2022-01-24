d43e142789189a6da292f6d7f117e90bbd7bc743
jorgehauck <jorge.hauck1997@outlook.com>
2022-01-23 21:42:32 -0300

    Rodapé v1.1

--- frontend/src/components/Rodape/index.tsx
+++ frontend/src/components/Rodape/index.tsx
@@ -5,7 +5,8 @@ function Footer() {
     return (
         <footer id="myFooter">
             <div className="container">
-                <p className="footer-copyright">© 2022 Copyright - Projeto Criado por: José Jorge Hauck Júnior</p>
+                <p className="footer-copyright">© 2022 Copyright - Projeto Criado por:</p>
+                <p className='footer-copyright '>José Jorge Hauck Júnior</p>
             </div>
             <div className="footer-social">
                 <a href="https://www.linkedin.com/in/jorgehauck/" target="_blank" rel="noreferrer"><img src={linkedin} alt="" className='linkedin'/></a>
