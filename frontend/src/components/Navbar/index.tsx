e07e6a3fe1b062d46cfc5976d8eae76787ff341e
jorgehauck <jorge.hauck1997@outlook.com>
2022-01-29 22:00:15 -0300

    Atualização de Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,15 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
+                    <Link to="/">
                     <h1>DSMovie</h1>
+                    </Link>
                     <a href="https://github.com/jorgehauck" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
